fa62a63e9495df9243087f02fcc7742765697b3f
jvnogueira <nogueirajva@gmail>
2023-09-27 09:17:22 -0300

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,5 @@
 
 
-// ================================================================================== /
-
 
 // == Componente de navbar 01 == //
 
@@ -17,7 +15,7 @@ class navbar01 extends HTMLElement {
 
 <menu-01></menu-01>
 
-<header class="bg-white/90">
+<header class="bg-white/95 fixed top-0 z-40 w-full">
 
   <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
 
@@ -83,8 +81,8 @@ class navbar01 extends HTMLElement {
 
       </div>
 
-      <div class="flex items-center gap-4 ">
-        <div class="hidden md:flex gap-4 ">
+      <div class="flex items-center gap-4 max-md:scale-95">
+        <div class="flex gap-4 ">
           <a
             class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow hover:scale-105"
             href="/"
@@ -92,7 +90,7 @@ class navbar01 extends HTMLElement {
             Entrar
           </a>
 
-          <div class="hidden sm:flex">
+          <div class="flex">
             <a
               class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 hover:scale-105"
               href="/"
@@ -103,7 +101,7 @@ class navbar01 extends HTMLElement {
           </div>
         </div>
 
-        <div class="block md:hidden">
+        <div class="hidden">
           <button
             id="iconNavbar"
             class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
@@ -151,6 +149,74 @@ customElements.define("navbar-01", navbar01);
 
 
 
+
+// == Componente de navbar 02 == //
+
+class navbar02 extends HTMLElement {
+  constructor() {
+    super();
+
+    this.innerHTML = `
+    
+          
+  
+<!-- Codigo -->
+
+<div class="z-40 fixed hidden max-md:block w-full bottom-0 bg-white/95 outline outline-1 outline-gray-300 rounded-md shadow-md">
+
+    <ul class="flex justify-around -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+       
+        <li class="mr-2">
+
+            <a href="#" class="flex flex-col gap-2 items-center justify-center p-4 text-teal-600  rounded-t-lg active hover:text-teal-600/60 group" aria-current="page">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>Início
+            </a>
+
+        </li>
+
+        <li class="mr-2">
+
+            <a href="#" class="flex flex-col gap-2 items-center justify-center p-4 rounded-t-lg hover:text-teal-600/60  group">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>Sobre
+            </a>
+
+        </li>
+
+
+        <li class="mr-2">
+
+            <a href="#" class="flex flex-col gap-2 items-center justify-center p-4 rounded-t-lg hover:text-teal-600/60 group">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" /></svg>Produtos
+
+            </a>
+
+        </li>
+
+        <li class="mr-2">
+
+            <a href="#" class="flex flex-col gap-2 items-center justify-center p-4  rounded-t-lg hover:text-teal-600/60 group">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" /></svg>Suporte
+            </a>
+            
+        </li>
+       
+    </ul>
+</div>
+          
+          
+<!-- Codigo -->
+        
+
+      `;
+    
+  }
+}
+
+customElements.define("navbar-02", navbar02);
+
+
+// ================================================================================== //
 
 
 
