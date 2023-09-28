b4125a52f3ba0ab262cc6e4a408d9b13654586b4
jvnogueira <nogueirajva@gmail>
2023-09-28 08:11:23 -0300

    update

--- components/Forms.js
+++ components/Forms.js
@@ -78,6 +78,40 @@ function validarSenha(idinputEmail,idalertainputemail) {
   });
 }
 
+// -------------
+
+// Campos obrigatorios
+
+function validarFormulario(formulario) {
+  
+  const blockinput = formulario.querySelectorAll('.blockinput');
+  
+
+  for (const iblockinput of blockinput) {
+
+    const camposObrigatorios = iblockinput.querySelector('.required');
+
+    const valorCampo = camposObrigatorios.value.trim();
+
+    if (valorCampo === '') {
+
+      const msgalert = iblockinput.querySelector('p');
+      msgalert.textContent = 'Campo Obrigatório';
+      msgalert.style.color = 'rgba(255, 0, 0, 0.5)';
+      msgalert.classList.remove('hidden');
+
+      camposObrigatorios.focus();
+
+      return false; // Impede o envio do formulário
+
+    }
+
+  }
+
+  return true; // Permite o envio do formulário se todos os campos obrigatórios estiverem preenchidos
+
+}
+
 
 // ===============================================================================
 
@@ -130,24 +164,24 @@ class formlogin extends HTMLElement {
                   Entre na sua conta
               </h1>
 
-              <form class="space-y-4 md:space-y-6">
+              <form id="formlogin" class="space-y-4 md:space-y-6">
 
-                  <div class="mb-6">
+                  <div class="mb-6 blockinput">
 
                       <label for="txtEmailLogin" class="block mb-2 text-sm font-medium text-gray-500 0 ">Email</label>
 
-                      <input autocomplete="off" type="email" id="txtEmailLogin" maxlength="100" class=" bg-gray-50 border border-gray-500 text-teal-500  placeholder-gray-300 text-sm rounded-lg focus:scale-105 block w-full p-2.5" placeholder="usuario@email.com"> 
+                      <input autocomplete="off" type="email" id="txtEmailLogin" maxlength="100" class="required bg-gray-50 border border-gray-500 text-gray-500  placeholder-gray-300 text-sm rounded-lg focus:scale-105 block w-full p-2.5" placeholder="usuario@email.com"> 
 
                       <p id="alertainputemail" class="hidden mt-2 text-sm text-gray-600 ">Email válido</p>
 
                   </div>
 
-                  <div class="mb-6">
+                  <div class="mb-6 blockinput">
 
                       <label for="txtSenhaLogin" class="block mb-2 text-sm font-medium text-gray-500 ">Senha</label>
 
-                      <div class="relative flex items-center">
-                      <input autocomplete="off" maxlength="12" type="password" id="txtSenhaLogin" class="bg-gray-50 border border-gray-500 text-teal-500  placeholder-gray-300 text-sm rounded-lg focus:scale-105 block w-full p-2.5" placeholder="••••••••">
+                      <div class="relative flex items-center ">
+                      <input autocomplete="off" maxlength="12" type="password" id="txtSenhaLogin" class="required bg-gray-50 border border-gray-500 text-gray-500  placeholder-gray-300 text-sm rounded-lg focus:scale-105 block w-full p-2.5" placeholder="••••••••">
                       <svg id="iconmostrarSenha" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" w-6 h-6 absolute right-4 text-gray-500 hover:text-teal-600 cursor-pointer"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                       <svg id="iconocultarSenha" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hidden w-6 h-6 absolute right-4 text-gray-500 hover:text-teal-600 cursor-pointer"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
 
@@ -180,7 +214,7 @@ class formlogin extends HTMLElement {
 
                   </div>
 
-                  <button type="button" class="w-full text-white bg-teal-600 hover:bg-teal-700 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:scale-105 hover:scale-105 ">Entrar</button>
+                  <button id="btnentrar" type="button" class="w-full text-white bg-teal-600 hover:bg-teal-700 outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:scale-105 hover:scale-105 ">Entrar</button>
 
                   <p class="text-sm font-light text-gray-500 ">
 
@@ -208,7 +242,13 @@ class formlogin extends HTMLElement {
 
         validarEmail('txtEmailLogin','alertainputemail');
 
-        validarSenha('txtSenhaLogin','alertainputsenha')
+        validarSenha('txtSenhaLogin','alertainputsenha');
+
+        const btnentrar = document.getElementById('btnentrar');
+
+        const formlogin = document.getElementById('formlogin');
+        
+        btnentrar.addEventListener('click',()=>{ validarFormulario(formlogin) })
 
     }
   }
