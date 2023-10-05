22a43f74358cc256575eca9d6d35c00736df841a
jvnogueira <nogueirajva@gmail>
2023-10-05 15:02:09 -0300

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -138,7 +138,7 @@ class navbar01 extends HTMLElement {
         <div class="flex items-center gap-8">
 
           <cart-01></cart-01>
-          <avatar-01></avatar-01>
+          <avatar-01 class="hidden"></avatar-01>
           
         </div>
 
@@ -189,7 +189,7 @@ class navbar02 extends HTMLElement {
        
         <li class="mr-2">
 
-            <a href="./index.html" class="flex flex-col gap-2 items-center justify-center p-4 text-${telainicio}-600  rounded-t-lg active hover:text-teal-600/60 group" aria-current="page">
+            <a href="../index.html" class="flex flex-col gap-2 items-center justify-center p-4 text-${telainicio}-600  rounded-t-lg active hover:text-teal-600/60 group" aria-current="page">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 fill-none"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>Início
             </a>
