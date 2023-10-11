732edb62ed7da43833c95a7d4c29972b89690cdc
jvnogueira <nogueirajva@gmail>
2023-10-11 12:17:19 -0300

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -210,7 +210,7 @@ class navbar02 extends HTMLElement {
 
         <li class="mr-2">
 
-            <a href="/html/produtos.html" class="flex flex-col gap-2 items-center justify-center p-4 text-${telaprodutos}-600 rounded-t-lg hover:text-teal-600/60 group">
+            <a href="https://jvdesigner.github.io/ecommerce/html/produtos.html" class="flex flex-col gap-2 items-center justify-center p-4 text-${telaprodutos}-600 rounded-t-lg hover:text-teal-600/60 group">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 fill-none"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" /></svg>Produtos
 
             </a>
